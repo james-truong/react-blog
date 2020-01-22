c36fe63cd195020118db034aff5351d6d320a9c8
James Truong <james.truong@hotmail.com>
2020-01-22 00:32:10 -0500

    added basic axios skeleton

--- client/src/hooks/home.js
+++ client/src/hooks/home.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Home = props => (
+const Home = props => {
+  useEffect(() => {
+    axios.get("/api/hello").then(res => setState(res.data));
+  }, []);
+
+  const [state, setState] = useState("");
+
+  return (
     <div>
       Home
+      <p>{state}</p>
     </div>
-);
+  );
+};
 
 export default Home;
--- server/main/app.js
+++ server/main/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
+var indexRouter = require('./routes')
 
 var app = express();
 
@@ -14,5 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use('/', indexRouter)
 
 module.exports = app;
